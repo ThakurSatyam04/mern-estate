a4da4f0a2dfdceacf60457a93f8e3e9db1601a83
SatyamKumar <kumarsatyam04.2000@gmail.com>
2023-11-01 17:06:36 +0530

    Design header component

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,13 @@ import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Profile from './pages/Profile'
 import About from './pages/About'
+import Header from './components/Header'
 
 function App() {
 
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path='/' element={<Home/>}></Route>
         <Route path='/sign-in' element={<SignIn/>}></Route>
