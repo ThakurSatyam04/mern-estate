53d0d79a0056ffbe48d5fe2d4905d56fb5b15cb4
SatyamKumar <kumarsatyam04.2000@gmail.com>
2023-11-05 15:58:57 +0530

    create get user listing api route

--- server/controllers/listingController.js
+++ server/controllers/listingController.js
@@ -2,7 +2,7 @@ import Listing from "../models/listingModel.js";
 
 export const createListing = async (req,res,next) => {
     try {
-        const listing = await Listing(req.body);
+        const listing = await Listing.create(req.body);
         return res.status(201).json(listing);
     } catch (error) {
         next(error);
