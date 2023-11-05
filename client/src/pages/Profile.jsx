f96f9db9dda459e20fa4e7dccacbbefcbc691b48
SatyamKumar <kumarsatyam04.2000@gmail.com>
2023-11-05 16:47:14 +0530

    complete delete user listing functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -121,6 +121,26 @@ const Profile = () => {
     }
   }
 
+  const handleListingDelete = async(listingId) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`,{
+        method: 'DELETE',
+      });
+      const data = res.json();
+      if(data.success === false){
+        console.log(data.message);
+        return;
+      }
+
+      // Update the userListing after deleting any list
+      setUserListings((prev) => prev.filter((listing)=> listing._id !== listingId))
+      
+    } catch (error) {
+      console.log(error.message);
+    }
+
+  }
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -199,7 +219,7 @@ const Profile = () => {
                 <p>{listing.name}</p>
               </Link>
               <div className='flex flex-col items-center'>
-                <button className='text-red-700 uppercase'>Delete</button>
+                <button onClick={()=>handleListingDelete(listing._id)} className='text-red-700 uppercase'>Delete</button>
                 <button className='text-green-700 uppercase'>Edit</button>
               </div>
             </div>
--- server/routers/listingRouter.js
+++ server/routers/listingRouter.js
@@ -1,9 +1,10 @@
 import express from 'express'
 import {verifyToken} from '../utils/verifyUser.js'
-import { createListing } from '../controllers/listingController.js';
+import { createListing, deleteListing} from '../controllers/listingController.js';
 
 const router = express.Router();
 
 router.post('/create',verifyToken, createListing)
+router.delete('/delete/:id', verifyToken, deleteListing)
 
 export default router;
