f866eecd9f67bc94132dcd66f855a3840a1d8c58
SatyamKumar <kumarsatyam04.2000@gmail.com>
2023-11-09 01:11:27 +0530

    made change in index.js

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ import userRouter from './routers/userRouter.js'
 import authRouter from './routers/authRouter.js'
 import listingRouter from './routers/listingRouter.js'
 import cookieParser from 'cookie-parser'
-import path from 'path';
+// import path from 'path';
 
 dotenv.config()
 const app = express();
@@ -19,7 +19,7 @@ mongoose.connect(process.env.MONGO_URL).then(()=>{
 })
 
 
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
 
 const PORT = process.env.PORT || 3000 
 
@@ -31,11 +31,17 @@ app.use('/api/user', userRouter)
 app.use('/api/auth', authRouter)
 app.use('/api/listing', listingRouter)
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
+// app.use(express.static(path.join(__dirname, '/client/dist')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-})
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+// })
+
+app.get('*',(req,res,next)=>{
+    res.status(200).json({
+      message:'bad request'
+    })
+  })
 
 // Creating a middleware to handle the errors
 
