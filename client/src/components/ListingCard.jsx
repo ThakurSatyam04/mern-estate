ecc53a2b8215f134eb3d5733977ecbfeadc5c8a3
SatyamKumar <kumarsatyam04.2000@gmail.com>
2023-11-08 23:55:22 +0530

    add show more listings functionality

--- client/src/components/ListingCard.jsx
+++ client/src/components/ListingCard.jsx
@@ -2,7 +2,7 @@ import {Link} from 'react-router-dom'
 import {MdLocationOn} from 'react-icons/md'
 const ListingCard = ({listing}) => {
   return (
-    <div className='bg-white shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg w-full sm:w-[330px]'>
+    <div className='bg-white shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg w-full sm:w-[250px]'>
       <Link to={`/show-listing/${listing._id}`}>
         <img src={listing.imageUrls[0]} alt="listing cover" className='h-[320px] sm:h-[220px] w-full object-cover hover:scale-105 transition-scale duration-300'/>
         <div className='p-3 flex flex-col gap-2 w-full'>
--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -15,8 +15,7 @@ const Search = () => {
     });
     const [loading,setLoading] = useState(false);
     const [listings, setListings] = useState([]);
-
-    console.log(listings)
+    const [showMore, setShowMore] = useState(false);
 
     useEffect(() => {
         const urlParams = new URLSearchParams(location.search);
@@ -50,9 +49,16 @@ const Search = () => {
     
         const fetchListings = async () => {
           setLoading(true);
+          setShowMore(false);
           const searchQuery = urlParams.toString();
           const res = await fetch(`/api/listing/get?${searchQuery}`);
           const data = await res.json();
+          if(data.length > 8){
+            setShowMore(true);
+          }
+          else{
+            setShowMore(false);
+          }
           setListings(data);
           setLoading(false);
         };
@@ -107,6 +113,20 @@ const Search = () => {
         const searchQuery = urlParams.toString();
         navigate(`/search?${searchQuery}`);
       };
+
+      const onShowMoreClick = async () => {
+        const numberOfListings = listings.length;
+        const startIndex = numberOfListings;
+        const urlParams = new URLSearchParams(location.search);
+        urlParams.set('startIndex', startIndex);
+        const searchQuery = urlParams.toString();
+        const res = await fetch(`/api/listing/get?${searchQuery}`);
+        const data = await res.json();
+        if(data.length < 9){
+          setShowMore(false);
+        }
+        setListings([...listings, ...data]);
+      }
 
   return (
     <div className="flex flex-col md:flex-row">
@@ -222,8 +242,17 @@ const Search = () => {
                 <p className="text-xl text-slate-700 text-center w-full">Loading...</p>
             )
         }
+        <div className="flex justify-center flex-wrap gap-4">
+          {
+              !loading && listings && listings.map((listing)=> <ListingCard key={listing._id} listing={listing}/>)
+          }
+        </div>
         {
-            !loading && listings && listings.map((listing)=> <ListingCard key={listing._id} listing={listing}/>)
+          showMore && (
+            <button onClick={onShowMoreClick} className="text-green-700 hover-underline py-7">
+              Show more
+            </button>
+          )
         }
         </div>
         
