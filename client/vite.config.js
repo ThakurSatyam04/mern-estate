b1ba2d851757b2a87f807185d8e60fc1bd8ddc79
SatyamKumar <kumarsatyam04.2000@gmail.com>
2023-11-09 18:00:02 +0530

    vercel.json added

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://mern-estate-gold.vercel.app/',
+        target: 'http://localhost:8000',
         secure: false,
       },
     },
