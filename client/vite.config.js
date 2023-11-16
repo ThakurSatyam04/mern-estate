67ad2dfa43a4e86b3f7a11e6805344ee80c2104c
SatyamKumar <kumarsatyam04.2000@gmail.com>
2023-11-17 00:37:27 +0530

    added backend link from vercel in vite.config.js file

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,8 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        // target: 'http://localhost:8000',
+        target: 'https://mern-estate-brown.vercel.app/',
         secure: false,
       },
     },
